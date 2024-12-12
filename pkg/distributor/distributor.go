f13e18ae2c0315a6f9728bd5003fe22cfeb8bfa0
Arve Knudsen <arve.knudsen@gmail.com>
2024-12-12 14:34:22 +0100

    Free query buffers also in case of success
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- pkg/distributor/distributor.go
+++ pkg/distributor/distributor.go
@@ -2704,6 +2704,11 @@ func (d *Distributor) MetricsForLabelMatchers(ctx context.Context, from, through
 	if err != nil {
 		return nil, err
 	}
+	defer func() {
+		for _, resp := range resps {
+			resp.FreeBuffer()
+		}
+	}()
 
 	metrics := map[uint64]labels.Labels{}
 	for _, resp := range resps {
@@ -2720,6 +2725,8 @@ func (d *Distributor) MetricsForLabelMatchers(ctx context.Context, from, through
 		if err := queryLimiter.AddSeries(m); err != nil {
 			return nil, err
 		}
+		// Make safe copies of labels.
+		m.InternStrings(strings.Clone)
 		result = append(result, m)
 	}
 	return result, nil
