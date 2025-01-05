5f0d88ffb988def7963b4b314c1761127d74ac34
Arve Knudsen <arve.knudsen@gmail.com>
2025-01-05 13:12:09 +0100

    ingester/client.QueryStreamResponse: Use memory pooling
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- pkg/distributor/distributor.go
+++ pkg/distributor/distributor.go
@@ -2712,7 +2712,7 @@ func (d *Distributor) MetricsForLabelMatchers(ctx context.Context, from, through
 	}
 	defer func() {
 		for _, resp := range resps {
-			resp.FreeBuffer()
+			resp.Release()
 		}
 	}()
 
--- pkg/distributor/query.go
+++ pkg/distributor/query.go
@@ -24,6 +24,7 @@ import (
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/model/labels"
 
+	"github.com/grafana/mimir/pkg/ingester/client"
 	ingester_client "github.com/grafana/mimir/pkg/ingester/client"
 	"github.com/grafana/mimir/pkg/mimirpb"
 	"github.com/grafana/mimir/pkg/querier/stats"
@@ -66,11 +67,13 @@ func (d *Distributor) QueryExemplars(ctx context.Context, from, to model.Time, m
 		if err != nil {
 			return err
 		}
+		/* TODO: Fix me.
 		defer func() {
 			for _, r := range results {
-				r.FreeBuffer()
+				r.Release()
 			}
 		}()
+		*/
 
 		result = mergeExemplarQueryResponses(results)
 
@@ -180,7 +183,7 @@ func mergeExemplarSets(a, b []mimirpb.Exemplar) []mimirpb.Exemplar {
 
 func mergeExemplarQueryResponses(results []*ingester_client.ExemplarQueryResponse) *ingester_client.ExemplarQueryResponse {
 	var keys []string
-	exemplarResults := make(map[string]mimirpb.TimeSeries)
+	exemplarResults := make(map[string]mimirpb.CustomTimeSeries)
 	for _, r := range results {
 		for _, ts := range r.Timeseries {
 			lbls := mimirpb.FromLabelAdaptersToKeyString(ts.Labels)
@@ -199,7 +202,7 @@ func mergeExemplarQueryResponses(results []*ingester_client.ExemplarQueryRespons
 	// Query results from each ingester were sorted, but are not necessarily still sorted after merging.
 	slices.Sort(keys)
 
-	result := make([]mimirpb.TimeSeries, len(exemplarResults))
+	result := make([]mimirpb.CustomTimeSeries, len(exemplarResults))
 	for i, k := range keys {
 		ts := exemplarResults[k]
 		for i, l := range ts.Labels {
@@ -220,8 +223,8 @@ func mergeExemplarQueryResponses(results []*ingester_client.ExemplarQueryRespons
 
 type ingesterQueryResult struct {
 	// Why retain the batches rather than build a single slice? We don't need a single slice for each ingester, so building a single slice for each ingester is a waste of time.
-	chunkseriesBatches [][]ingester_client.TimeSeriesChunk
-	timeseriesBatches  [][]mimirpb.TimeSeries
+	chunkseriesBatches [][]ingester_client.CustomTimeSeriesChunk
+	timeseriesBatches  [][]mimirpb.CustomTimeSeries
 	streamingSeries    seriesChunksStream
 }
 
@@ -327,8 +330,8 @@ func (d *Distributor) queryIngesterStream(ctx context.Context, replicationSets [
 		queryMetrics.IngesterChunksTotal.Add(float64(totalChunks))
 	}()
 
-	hashToChunkseries := map[string]ingester_client.TimeSeriesChunk{}
-	hashToTimeSeries := map[string]mimirpb.TimeSeries{}
+	hashToChunkseries := map[string]ingester_client.CustomTimeSeriesChunk{}
+	hashToTimeSeries := map[string]mimirpb.CustomTimeSeries{}
 
 	for _, res := range results {
 		// Accumulate any chunk series
@@ -336,6 +339,9 @@ func (d *Distributor) queryIngesterStream(ctx context.Context, replicationSets [
 			for _, series := range batch {
 				key := mimirpb.FromLabelAdaptersToKeyString(series.Labels)
 				existing := hashToChunkseries[key]
+				if existing.TimeSeriesChunk == nil {
+					existing.TimeSeriesChunk = &client.TimeSeriesChunk{}
+				}
 				existing.Labels = series.Labels
 
 				numPotentialChunks := len(existing.Chunks) + len(series.Chunks)
@@ -370,8 +376,8 @@ func (d *Distributor) queryIngesterStream(ctx context.Context, replicationSets [
 
 	// Now turn the accumulated maps into slices.
 	resp := ingester_client.CombinedQueryStreamResponse{
-		Chunkseries:     make([]ingester_client.TimeSeriesChunk, 0, len(hashToChunkseries)),
-		Timeseries:      make([]mimirpb.TimeSeries, 0, len(hashToTimeSeries)),
+		Chunkseries:     make([]ingester_client.CustomTimeSeriesChunk, 0, len(hashToChunkseries)),
+		Timeseries:      make([]mimirpb.CustomTimeSeries, 0, len(hashToTimeSeries)),
 		StreamingSeries: mergeSeriesChunkStreams(results, d.estimatedIngestersPerSeries(replicationSets)),
 	}
 	for _, series := range hashToChunkseries {
@@ -395,7 +401,6 @@ func receiveResponse(stream ingester_client.Ingester_QueryStreamClient, streamin
 	if err != nil {
 		return 0, nil, false, err
 	}
-	defer resp.FreeBuffer()
 
 	if len(resp.Timeseries) > 0 {
 		for _, series := range resp.Timeseries {
