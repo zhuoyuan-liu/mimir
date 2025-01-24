83094a68d0ce02dba1d946cda69be997ce4740df
Yuri Nikolic <durica.nikolic@grafana.com>
2025-01-24 19:41:31 +0100

    Introduce HelloWorld gRPC Middleware
    Signed-off-by: Yuri Nikolic <durica.nikolic@grafana.com>

---
+++ vendor/github.com/grafana/dskit/middleware/grpc_hello_world.go
@@ -0,0 +1,68 @@
+package middleware
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
+	"github.com/grafana/dskit/httpgrpc"
+	"google.golang.org/grpc"
+)
+
+const (
+	messageKey = "message"
+)
+
+func HelloWorldUnaryServerInterceptor(logger log.Logger) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		reqType := requestToString(req)
+		level.Info(logger).Log("server", info.Server, "method", info.FullMethod, "msg", "Hello, World!!!", "req", reqType)
+		return handler(ctx, req)
+	}
+}
+
+func HelloWorldStreamServerInterceptor(logger log.Logger) grpc.StreamServerInterceptor {
+	return func(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+		streamType := make([]string, 0, 2)
+		if info.IsServerStream {
+			streamType = append(streamType, "stream")
+		}
+		if info.IsClientStream {
+			streamType = append(streamType, "client")
+		}
+		level.Info(logger).Log("stream", streamType, "method", info.FullMethod, "msg", "Hello, World!!!")
+		return handler(srv, ss)
+	}
+}
+
+func requestToString(req interface{}) string {
+	httpGrpcReq, ok := req.(*httpgrpc.HTTPRequest)
+	if ok {
+		return httpGrpcReq.String()
+	}
+	httpReq, ok := req.(*http.Request)
+	if ok {
+		repeatedStringForHeaders := "[]*Header{"
+		for key, values := range httpReq.Header {
+			s := strings.Join([]string{`&Header{`,
+				`Key:` + fmt.Sprintf("%v", key) + `,`,
+				`Values:` + fmt.Sprintf("%v", values) + `,`,
+				`}`,
+			}, "")
+			repeatedStringForHeaders += s + ","
+		}
+		repeatedStringForHeaders += "}"
+		s := strings.Join([]string{`&HTTPRequest{`,
+			`Method:` + fmt.Sprintf("%v", httpReq.Method) + `,`,
+			`Url:` + fmt.Sprintf("%v", httpReq.URL) + `,`,
+			`Headers:` + repeatedStringForHeaders + `,`,
+			`Body:` + fmt.Sprintf("%v", httpReq.Body) + `,`,
+			`}`,
+		}, "")
+		return s
+	}
+	return "unknown"
+}
